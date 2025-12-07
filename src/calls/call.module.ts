2e6be3145851b1d1df9c0340ab2f3f224b05d793
Samuil Stoyanov <atanas872@gmail.com>
2025-12-07 16:30:37 +0200

    calls

--- src/calls/call.module.ts
+++ src/calls/call.module.ts
@@ -7,6 +7,7 @@ import { User } from '../users/entities/user.entity';
 import { Ambulance } from '../ambulances/entities/ambulance.entity';
 import { AmbulancesModule } from '../ambulances/ambulance.module';
 import { HospitalsModule } from '../hospitals/hospitals.module';
+import { HospitalsModule } from '../hospitals/hospitals.module';
 import { GoogleMapsService } from '../common/services/google-maps.service';
 import { RealtimeModule } from '../realtime/realtime.module';
 
