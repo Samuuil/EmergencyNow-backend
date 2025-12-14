5cc8cccb8eec862767dc505b6265269f327b7cd8
Samuuil <atanas872@gmail.com>
2025-12-14 21:16:23 +0200

    redis integration for verification codes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/user.module';
 import { StateArchiveModule } from '../state-archive/state-archive.module';
-import { VerificationCode } from './entities/verification-code.entity';
+import { RedisModule } from '../common/redis/redis.module';
 import { MailService } from './services/mail.service';
 import { SmsService } from './services/sms.service';
+import { VerificationCodeService } from './services/verification-code.service';
 
 @Module({
   imports: [
@@ -18,7 +18,7 @@ import { SmsService } from './services/sms.service';
     UsersModule,
     StateArchiveModule,
     PassportModule,
-    TypeOrmModule.forFeature([VerificationCode]),
+    RedisModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
@@ -28,7 +28,7 @@ import { SmsService } from './services/sms.service';
       }),
     }),
   ],
-  providers: [AuthService, JwtStrategy, MailService, SmsService],
+  providers: [AuthService, JwtStrategy, MailService, SmsService, VerificationCodeService],
   controllers: [AuthController],
   exports: [AuthService, MailService],
 })
